5523dfe87fe39318089fc0defda2dadf7f242f9d
rafi021 <mahmud.ibrahim021@gmail.com>
2022-08-05 18:37:30 +0600

    add category index and create page on menu bar

--- resources/views/backend/layouts/inc/menu.blade.php
+++ resources/views/backend/layouts/inc/menu.blade.php
@@ -2,10 +2,28 @@
   <div class="menu-container flex-grow-1">
     <ul id="menu" class="menu">
         <li>
-            <a href="Dashboard.html">
+            <a href="{{ route('admin.dashboard') }}">
                 <i data-cs-icon="shop" class="icon" data-cs-size="18"></i>
                 <span class="label">Dashboard</span>
             </a>
+        </li>
+        <li>
+            <a href="#categories" data-href="">
+                <i data-cs-icon="cupcake" class="icon" data-cs-size="18"></i>
+                <span class="label">Categories</span>
+            </a>
+            <ul id="categories">
+                <li>
+                    <a href="{{ route('category.index') }}">
+                        <span class="label">List</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('category.create') }}">
+                        <span class="label">Add New</span>
+                    </a>
+                </li>
+            </ul>
         </li>
         <li>
             <a href="#products" data-href="Products.html">
