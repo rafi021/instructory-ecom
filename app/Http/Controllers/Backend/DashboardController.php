3c0e03ba8ddcfaf4b0a6e15f4a0dec1e247ec90c
rafi021 <mahmud.ibrahim021@gmail.com>
2022-09-14 06:29:36 +0600

    Analytics report on dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -3,12 +3,38 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Order;
+use App\Models\OrderDetails;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('backend.pages.Dashboard');
+        $total_earning = Order::sum('total');
+        $total_order_count = Order::count();
+        $total_categories = Category::count();
+        $total_products = Product::count();
+        $total_customers = User::where('role_id', 2)->count();
+        $orders = Order::with(['billing', 'orderdetails'])->latest('id')->paginate(15);
+
+        $order_on_2020 = Order::whereBetween('created_at', ['2020-01-01', '2020-12-31'])->count();
+        $order_on_2021 = Order::whereBetween('created_at', ['2021-01-01', '2021-12-31'])->count();
+        $order_on_2022 = Order::whereBetween('created_at', ['2022-01-01', '2022-12-31'])->count();
+
+        $order_yearwise = array($order_on_2020, $order_on_2021, $order_on_2022);
+
+        return view('backend.pages.Dashboard', compact(
+            'total_earning',
+            'total_order_count',
+            'total_categories',
+            'total_products',
+            'total_customers',
+            'orders',
+            'order_yearwise',
+        ));
     }
 }
